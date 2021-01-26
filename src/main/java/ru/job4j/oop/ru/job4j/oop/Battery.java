3b94debffbeb9e9338cd0e341e61731081fd4834
MaratKhatipov <mmmarat86@gmail.com>
2021-01-26 19:10:30 +0500

    1.8. Состояние объекта [#442849]

---
+++ src/main/java/ru/job4j/oop/ru/job4j/oop/Battery.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop.ru.job4j.oop;
+
+public class Battery {
+	private int load;
+
+	public Battery(int charge) {
+		this.load = charge;
+	}
+
+	public void exchange(Battery another) {
+		another.load = another.load + this.load;
+		this.load = 0;
+		if (another.load > 100) {
+			another.load = 100;
+			System.out.println("Your battery is full");
+		}
+	}
+
+	public static void main(String[] args) {
+		Battery first = new Battery(100);
+		Battery second = new Battery(5);
+		System.out.println("Battery 1 = " + first.load + " Battery 2 = " + second.load);
+		System.out.println();
+		first.exchange(second);
+		System.out.println("Battery 1 = " + first.load + " Battery 2 = " + second.load);
+	}
+}
