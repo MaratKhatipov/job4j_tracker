70d81f7461a94ffb63191325f9c2cec216f551c3
MaratKhatipov <mmmarat86@gmail.com>
2021-01-26 16:42:20 +0500

    1.2. Вызов метода объекта [#442845]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -6,11 +6,13 @@ public class Student {
 			System.out.println("Tra tra tra");
 		}
 	}
+
 	public void song() {
 		for (int i = 0; i < 3; i++) {
 			System.out.println("I believe I can fly");
 		}
 	}
+
 	public static void main(String[] args) {
 		Student petya = new Student();
 		petya.music();
