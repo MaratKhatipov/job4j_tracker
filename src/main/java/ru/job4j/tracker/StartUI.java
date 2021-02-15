360cfed241ed1f944c3f831c2d916b5e7cb8be17
MaratKhatipov <mmmarat86@gmail.com>
2021-02-15 08:59:38 +0500

    9. Написать тесты на StartUI. [#442805]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -18,36 +18,6 @@ public class StartUI {
 		}
 	}
 
-	public static void findById(Input input, Tracker tracker) {
-		System.out.println("4. Find item by Id"
-				+ System.lineSeparator()
-				+ "Enter id to FIND:");
-		int id = input.askInt("Ent ID: ");
-		Item findId = tracker.findById(id);
-		if (tracker.findById(id) != null) {
-			System.out.println("Result find by id: " + findId);
-		} else {
-			System.out.println("Error. Not found id: " + id);
-		}
-	}
-
-	public static void findByName(Input input, Tracker tracker) {
-		System.out.println("Find items by name"
-				+ System.lineSeparator()
-				+ "Enter name to FIND:");
-		String findName = input.askStr("enter name to find");
-		Item[] resultFindName = tracker.findByName(findName);
-		if (resultFindName.length > 0) {
-			for (Item result : resultFindName) {
-				System.out.println("Result find by name - "
-						+ findName
-						+ result);
-			}
-		} else {
-			System.out.println("Result not found: " + findName);
-		}
-	}
-
 	public static void main(String[] args) {
 		Input input = new ConsoleInput();
 		Tracker tracker = new Tracker();
