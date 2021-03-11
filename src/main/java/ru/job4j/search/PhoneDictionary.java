2e167771377c846035ceecc36a1615d09505a657
MaratKhatipov <mmmarat86@gmail.com>
2021-03-11 19:51:03 +0500

    1. Телефонный справочник на базе ArrayList [#442771]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,37 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+	private ArrayList<Person> persons = new ArrayList<>();
+
+	public void add(Person persons) {
+		this.persons.add(persons);
+	}
+
+	public ArrayList<Person> find(String key) {
+		ArrayList<Person> result = new ArrayList<>();
+		for (Person person : persons) {
+			if (
+					person.getName().contains(key)
+					||
+					person.getSurname().contains(key)
+					||
+					person.getPhone().contains(key)
+					||
+					person.getAddress().contains(key)
+			) {
+				result.add(person);
+			}
+
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		PhoneDictionary phones = new PhoneDictionary();
+		phones.add(
+				new Person("Petr", "Arsentev", "534872", "Bryansk"));
+		ArrayList<Person> persons = phones.find("12");
+	}
+}
---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PhoneDictionaryTest {
+
+	@Test
+	public void whenFindByName() {
+		PhoneDictionary phones = new PhoneDictionary();
+		phones.add(
+				new Person("Petr", "Arsentev", "534872", "Bryansk")
+		);
+		ArrayList<Person> persons = phones.find("Pe");
+		assertThat(persons.get(0).getSurname(), is("Arsentev"));
+	}
+
+	@Test
+	public void whenFindByPhone() {
+		PhoneDictionary phones = new PhoneDictionary();
+		phones.add(
+				new Person("Petr", "Arsentev", "534872", "Bryansk")
+		);
+		ArrayList<Person> persons = phones.find("48");
+		assertThat(persons.get(0).getAddress(), is("Bryansk"));
+	}
+
+	@Test
+	public void whenNotFindBy() {
+		PhoneDictionary phones = new PhoneDictionary();
+		phones.add(
+				new Person("Petr", "Arsentev", "534872", "Bryansk")
+		);
+		ArrayList<Person> persons = phones.find("12");
+		int res = persons.size();
+		assertThat(res, is(0));
+	}
+}
