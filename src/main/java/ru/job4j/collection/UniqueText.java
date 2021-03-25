a597be3e17394021f071deb95cc5c00a6e2e3d51
MaratKhatipov <mmmarat86@gmail.com>
2021-03-25 15:57:28 +0500

    2. Идентичные тексты [#442785] исправленно

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -5,31 +5,17 @@ import java.util.HashSet;
 
 public class UniqueText {
 	public static boolean isEquals(String originText, String duplicateText) {
-		var origin = originText.split(" ");
-		var text = duplicateText.split(" ");
-		var check = new HashSet<>(Arrays.asList(origin));
-		for (String indexDuplicateText :text) {
-			if (!check.contains(indexDuplicateText)) {
-				return false;
-			}
-		}
-		return true;
-	}
-}
-
-/*
- 		boolean result = true;
 		String[] origin = originText.split(" ");
 		String[] text = duplicateText.split(" ");
 		HashSet<String> check = new HashSet<>();
 		for (String indexText : origin) {
 			check.add(indexText);
 		}
-		for (String indexDuplicateText :text) {
-			if (!check.contains(indexDuplicateText)) {
-				result = false;
+		for (String index :text) {
+			if (!check.contains(index)) {
 				return false;
 			}
 		}
 		return true;
-*/
+	}
+}
