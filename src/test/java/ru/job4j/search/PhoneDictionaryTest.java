c844c305d1eee7e70a31e8fc9dedae3421a9c7ed
MaratKhatipov <mmmarat86@gmail.com>
2021-04-14 16:55:33 +0500

    2. Local-Variable Type Inference [#442616]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -11,46 +11,46 @@ public class PhoneDictionaryTest {
 
 	@Test
 	public void whenFindByName1() {
-		PhoneDictionary phones = new PhoneDictionary();
+		var phones = new PhoneDictionary();
 		phones.add(
 				new Person("Petr", "Arsentev",
 						"534872", "Bryansk")
 		);
-		ArrayList<Person> persons = phones.find("Petr");
+		var persons = phones.find("Petr");
 		assertThat(persons.get(0).getSurname(), is("Arsentev"));
 	}
 
 	@Test
 	public void whenFindByName() {
-		PhoneDictionary phones = new PhoneDictionary();
+		var phones = new PhoneDictionary();
 		phones.add(
 				new Person("Petr", "Arsentev",
 						"534872", "Bryansk")
 		);
-		ArrayList<Person> persons = phones.find("Pe");
+		var persons = phones.find("Pe");
 		assertThat(persons.get(0).getSurname(), is("Arsentev"));
 	}
 
 	@Test
 	public void whenFindByPhone() {
-		PhoneDictionary phones = new PhoneDictionary();
+		var phones = new PhoneDictionary();
 		phones.add(
 				new Person("Petr", "Arsentev",
 						"534872", "Bryansk")
 		);
-		ArrayList<Person> persons = phones.find("48");
+		var persons = phones.find("48");
 		assertThat(persons.get(0).getAddress(), is("Bryansk"));
 	}
 
 	@Test
 	public void whenNotFindBy() {
-		PhoneDictionary phones = new PhoneDictionary();
+		var phones = new PhoneDictionary();
 		phones.add(
 				new Person("Petr", "Arsentev",
 						"534872", "Bryansk")
 		);
 		ArrayList<Person> persons = phones.find("12");
-		int res = persons.size();
+		var res = persons.size();
 		assertThat(res, is(0));
 	}
 }
